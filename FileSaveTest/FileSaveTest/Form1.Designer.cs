72cff62185cb6420b72d9c7fdf691bbd80c0d83b
kimmj272 <rlaaudwls5220@gmail.com>
2024-11-29 20:20:10 +0900

    test

---
+++ FileSaveTest/FileSaveTest/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace FileSaveTest
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbNumber = new System.Windows.Forms.TextBox();
+            this.tbNickName = new System.Windows.Forms.TextBox();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "실행결과";
+            // 
+            // txtResult
+            // 
+            this.txtResult.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.txtResult.Location = new System.Drawing.Point(28, 67);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(628, 333);
+            this.txtResult.TabIndex = 1;
+            this.txtResult.Text = "결과";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(69, 435);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(133, 45);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(276, 435);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(133, 45);
+            this.btnLoad.TabIndex = 3;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(475, 435);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(133, 45);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(725, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "이름";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(667, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 29);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "전화번호";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(725, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 29);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "별명";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(696, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 29);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "이메일";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(802, 64);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(297, 41);
+            this.tbName.TabIndex = 9;
+            // 
+            // tbNumber
+            // 
+            this.tbNumber.Location = new System.Drawing.Point(802, 115);
+            this.tbNumber.Name = "tbNumber";
+            this.tbNumber.Size = new System.Drawing.Size(297, 41);
+            this.tbNumber.TabIndex = 10;
+            // 
+            // tbNickName
+            // 
+            this.tbNickName.Location = new System.Drawing.Point(802, 167);
+            this.tbNickName.Name = "tbNickName";
+            this.tbNickName.Size = new System.Drawing.Size(297, 41);
+            this.tbNickName.TabIndex = 11;
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Location = new System.Drawing.Point(802, 227);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(297, 41);
+            this.tbEmail.TabIndex = 12;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1138, 509);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.tbNickName);
+            this.Controls.Add(this.tbNumber);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("굴림", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Margin = new System.Windows.Forms.Padding(7);
+            this.Name = "FrmMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txtResult;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbNumber;
+        private System.Windows.Forms.TextBox tbNickName;
+        private System.Windows.Forms.TextBox tbEmail;
+    }
+}
+
---
+++ FileSaveTest/FileSaveTest/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileSaveTest
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveTextFile();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            LoadTextFile();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtResult.Text = "";
+        }
+
+        private void SaveTextFile()
+        {
+            bool bCheck = true;
+            int nCount = 50;
+            float fValue = 3.14f;
+            string str = "안녕하세요";
+
+            StreamWriter fw = new StreamWriter("textdata1.txt");
+
+            fw.WriteLine(bCheck);
+            fw.WriteLine(nCount);
+            fw.WriteLine(fValue);
+            fw.WriteLine(str);
+
+            fw.Close();
+        }
+
+        private void LoadTextFile()
+        {
+            txtResult.Text = ""; 
+
+            StreamReader fr = new StreamReader("textdata1.txt");
+
+            string sCheck = fr.ReadLine();
+            string sCount = fr.ReadLine();
+            string sValue = fr.ReadLine();
+            string sStr = fr.ReadLine();
+
+            fr.Close();
+
+            bool bCheck = bool.Parse(sCheck);
+            int nCount = int.Parse(sCount);
+            float fValue = float.Parse(sValue);
+            string str = sStr;
+
+            txtResult.Text += ("Check = " + bCheck + "\n");
+            txtResult.Text += ("Count = " + nCount + "\n");
+            txtResult.Text += ("Value = " + fValue + "\n");
+            txtResult.Text += ("Str = " + str + "\n");
+        }
+
+        private void SaveInform()
+        {
+            StreamWriter fw = new StreamWriter("textdata1.txt");
+
+            fw.WriteLine(tbName.Text);
+            fw.WriteLine(tbNumber.Text);
+            fw.WriteLine(tbNickName.Text);
+            fw.WriteLine(tbEmail.Text);
+
+            fw.Close();
+        }
+    }
+}
